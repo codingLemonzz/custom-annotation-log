0201815855aa893bb04968545350346c68a34958
codinglemon <zry15671554200@qq.com>
2022-01-07 15:56:46 +0800

    git 测试

---
+++ src/main/java/cn/codinglemon/annolog/config/GitTest.java
@@ -0,0 +1,12 @@
+package cn.codinglemon.annolog.config;
+
+/**
+ * @author zry
+ * @date 2022-1-7 15:53
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+        System.out.println("zzz");
+    }
+}
