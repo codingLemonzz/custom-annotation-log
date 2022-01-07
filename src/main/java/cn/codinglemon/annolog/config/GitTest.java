493a9473df5b0147535a7d34950d7131a91c55b8
codinglemon <zry15671554200@qq.com>
2022-01-07 16:04:46 +0800

    git dev first commit

--- src/main/java/cn/codinglemon/annolog/config/GitTest.java
+++ src/main/java/cn/codinglemon/annolog/config/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("zzz");
         System.out.println("zzz");
+        System.out.println("dev zzz");
     }
 }
